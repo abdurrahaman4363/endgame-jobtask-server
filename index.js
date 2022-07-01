2d6983e17d6c4f30250da7bacaea588539328009
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-07-01 12:03:18 +0600

    create a update api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //maddleware
@@ -28,6 +28,31 @@ async function run(){
             const tasks = await cursor.toArray();
             res.send(tasks);
 
+        })
+        app.get('/task/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const tasks =await taskCollection.findOne(query);
+            res.send(tasks);
+
+        })
+
+        app.post('/task', async (req, res) => {
+            const addItem = req.body;
+            const result = await taskCollection.insertOne(addItem);
+            res.send(result);
+        })
+        app.put('/task/:id', async (req, res) => {
+            const id = req.params.id;
+            const update = req.body;
+            const filter = { _id:ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: update,
+            };
+            const result = await taskCollection.updateOne(filter, updateDoc, options);
+            
+            res.send(result);
         })
 
     }
