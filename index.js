acb107b7e3232650ce7fc8b0eb04e3465080a66d
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-07-01 06:45:58 +0600

    create a get api

--- index.js
+++ index.js
@@ -14,12 +14,35 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j7edy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
+/* client.connect(err => {
   const collection = client.db("test").collection("devices");
   console.log('I am from mongodb')
   // perform actions on the collection object
   client.close();
-});
+}); */
+
+async function run(){
+     
+    try{
+        await client.connect();
+        console.log('Database is connected');
+        const taskCollection = client.db('endgame_jobtask').collection('tasks');
+
+        app.get('/task', async(req, res)=>{
+            const query = {};
+            const cursor = taskCollection.find(query);
+            const tasks = await cursor.toArray();
+            res.send(tasks);
+
+        })
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
