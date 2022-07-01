f7991b8cb20e29e98aea9d079df6c4d67777e13a
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-07-01 08:07:54 +0600

    adding steps

--- index.js
+++ index.js
@@ -14,12 +14,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j7edy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-/* client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('I am from mongodb')
-  // perform actions on the collection object
-  client.close();
-}); */
 
 async function run(){
      
