178af89c16ff7987597cc153bfe046a9bea1d803
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-07-01 06:30:34 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -3,11 +3,24 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //maddleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j7edy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('I am from mongodb')
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', (req, res) => {
   res.send('This is from endgame');
